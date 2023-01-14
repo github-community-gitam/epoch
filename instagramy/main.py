d21ad91dc17476ef9b2d99eba6fb58d076588d92
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2023-01-14 23:59:23 +0530

    refactor script

--- instagramy/main.py
+++ instagramy/main.py
@@ -22,9 +22,6 @@ for index, post in enumerate(posts):
     lst.append(dic)
 data = json.dumps(lst, default=str)
 
-url = 'https://instagram.fhyd14-1.fna.fbcdn.net/v/t51.2885-15/322322322_1010390713682338_7672465270543721211_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fhyd14-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=WfeQUwMuLMMAX-gO2IO&edm=ABfd0MgBAAAA&ccb=7-5&oh=00_AfCUiUSUR1l1jGeZ_Z9b9_Ez6pNu607GeLrJ-lNkXjnnBw&oe=63BBCA5C&_nc_sid=7bff83'
-response = requests.get(url)
-
 
 filePath = os.path.join(folderPath, 'data.json')
 f = open(filePath, 'w')
